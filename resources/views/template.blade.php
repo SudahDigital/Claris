d153b5f539453b9c89c81d7c1d68e95fccf0cb14
Diannurul <diannurul05@gmail.com>
2021-02-16 17:12:23 +0700

    update

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -108,7 +108,8 @@
             font-weight: 800; 
             position: absolute;
             top: 80px;
-            left: 0px;
+            left: 20px;
+            right: 90px;
         }
 
         .button_welcome:hover {
